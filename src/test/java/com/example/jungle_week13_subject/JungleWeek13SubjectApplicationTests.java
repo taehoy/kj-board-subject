97d0c8d63c74cb8ef1f20b523b643a5736b87f75
taehoy <rethnoly@gmail.com>
2024-12-10 17:02:19 +0900

    init project

---
+++ src/test/java/com/example/jungle_week13_subject/JungleWeek13SubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jungle_week13_subject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungleWeek13SubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
